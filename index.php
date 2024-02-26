1688d9ed2c7c048c160474141a3734590f9d54a9
Richard Gabb <richardjgabb@gmail.com>
2024-02-26 12:26:59 +0000

    show database on homepage

--- index.php
+++ index.php
@@ -4,9 +4,12 @@ $db = new PDO('mysql:host=db;dbname=passportStampsDatabase', 'root', 'password')
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-$query = $db->prepare('SELECT `airport`,`country`, `date`, `image`
-FROM `stamps`;');
+$query = $db->prepare('SELECT `airport`,`country`, `date`, `image`, `airportStarRating`, `review`
+FROM `stamps`');
 $query->execute();
+
+$database = $query->fetchAll();
+
 ?>
 
 <!DOCTYPE html>
@@ -18,6 +21,17 @@ $query->execute();
     <header>
         <h1>Header</h1>
     </header>
+
+    <?php
+    foreach ($database as $item){
+        echo 'Airport: ' . $item['airport'] .'<br>';
+        echo 'Country: ' . $item['country'] .'<br>';
+        echo 'Date visited: ' . $item['date'] .'<br>';
+        echo 'Image: ' . $item['image'] .'<br>';
+        echo 'Rating: ' . $item['airportStarRating'] .'<br>';
+        echo 'Review: ' . $item['review'] .'<br><hr>';
+    }
+    ?>
 
 </body>
 </html>
