38b1afdb623da6889b84d8b1785f282a1b6df578
Richard Gabb <richardjgabb@gmail.com>
2024-02-27 11:49:52 +0000

    Story 1 complete

--- index.php
+++ index.php
@@ -1,37 +1,87 @@
 <?php
 
+if (!isset($_POST['airport'])) {
+
+    $airport = $_POST['airport'];
+    $country = $_POST['country'];
+    $date = $_POST['date'];
+    $image = $_POST['image'];
+    $rating = $_POST['rating'];
+    $review = $_POST['review'];
+
+    $db = new PDO('mysql:host=db;dbname=passportStampsDatabase', 'root', 'password');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+    $query = $db->prepare('INSERT INTO `stamps` (`airport`, `country`, `date`, `image`, `rating`, `review`)
+    VALUES (:airport, :country, :date, :image, :rating, :review);');
+
+    $query->bindParam(':airport', $airport);
+    $query->bindParam(':country', $country);
+    $query->bindParam(':date', $date);
+    $query->bindParam(':image', $image);
+    $query->bindParam(':rating', $rating);
+    $query->bindParam(':review', $review);
+
+    $query->execute();
+}
+
 $db = new PDO('mysql:host=db;dbname=passportStampsDatabase', 'root', 'password');
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-$query = $db->prepare('SELECT `airport`,`country`, `date`, `image`, `airportStarRating`, `review`
+$query = $db->prepare('SELECT `airport`,`country`, `date`, `image`, `rating`, `review`
 FROM `stamps`');
 $query->execute();
 
 $database = $query->fetchAll();
+
 
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
     <header>
-        <h1>Header</h1>
+        <div class="tripTitleDiv">
+            <img id="logoImage" src="images/TRIP-Photoroom.png-Photoroom.png" alt="Trip Logo">
+        </div>
     </header>
 
-    <?php
-    foreach ($database as $item){
-        echo 'Airport: ' . $item['airport'] .'<br>';
-        echo 'Country: ' . $item['country'] .'<br>';
-        echo 'Date visited: ' . $item['date'] .'<br>';
-        echo 'Image: ' . $item['image'] .'<br>';
-        echo 'Rating: ' . $item['airportStarRating'] .'<br>';
-        echo 'Review: ' . $item['review'] .'<br><hr>';
-    }
-    ?>
+    <section class="filterDiv">
+        <select name="filterBy" id="filterBy">
+            <option value="date">Date</option>
+            <option value="country">Country</option>
+            <option value="rating">Rating</option>
+        </select>
+        <a href="newEntry.php">Add Entry</a>
+        <select name="sortBy" id="sortBy">
+            <option value="dateNewest">Date, newest first</option>
+            <option value="dateOldest">Date, oldest first</option>
+            <option value="ratingAscending">Rating, ascending</option>
+            <option value="ratingDescending">Rating, descending</option>
+        </select>
+    </section>
 
+    <main>
+        <div class="passportPage">
+        <div class="passportDiv">
+            <?php
+            foreach ($database as $item){
+                $rotation = rand(-20, 20);
+                echo "<div class='stamp'>
+                <img src='{$item['image']}' style='rotate: {$rotation}deg'>"  .'<br></div>';
+//                echo '<p class="stampInfoTag">' . $item['country'] . ': '. $item['date'] . '</p></div>';
+            }
+            ?>
+
+        </div>
+        </div>
+    </main>
 </body>
 </html>
---
+++ newEntry.php
@@ -0,0 +1,35 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+<header>
+    <div class="tripTitleDiv">
+        <img id="logoImage" src="images/TRIP-Photoroom.png-Photoroom.png" alt="Trip Logo">
+    </div>
+</header>
+<section class="passportPage">
+    <form action="index.php" method="post">
+        <input type="text" name="airport" placeholder="Airport:">
+        <input type="text" name="country" placeholder="Country:">
+        <input type="date" name="date" placeholder="Date:">
+        <input type="text" name="image" placeholder="Passport stamp:">
+        <input type="number" min="1" max="5" name="rating" placeholder="Rating:">
+        <input type="text" name="review" placeholder="Review:">
+        <input type="submit">
+        <button onclick="history.go(-1)">Back</button>
+    </form>
+</section>
+
+
+
+</body>
+</html>
